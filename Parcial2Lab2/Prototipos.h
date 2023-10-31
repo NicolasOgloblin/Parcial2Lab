a8441b140fc2ab117199e6937b7f67d1cb4cbfe1
Nicolas <nicolas.ogloblin.w@gmail.com>
2023-10-31 14:18:32 -0300

    - Agrega prototipos ordenados

--- Parcial2Lab2/Prototipos.h
+++ Parcial2Lab2/Prototipos.h
@@ -7,38 +7,71 @@
 
 ///  ----------------------------------------------------  PROTOTIPOS  ----------------------------------------------------  ///
 
-void cargarCadena(char, int);
-int buscarPorDni(int);
-Jugadores leerRegistro (int);
-bool agregarJugador();
-bool agregarJugadorValidado();
-bool listarTodos ();
-bool listarPorDni ();
-bool modificarFechaInscripcion ();
-bool bajaLogica();
-bool buscarPorNombre ();
-bool modificarRegistro (Jugadores, int);
-void obtenerHoraSys();
-void subMenuDeportes();
+//Main
 void subMenuJugadores();
+void subMenuDeportes();
+void subMenuEquipos();
+void subMenuTiposDeDeporte();
 void subMenuReportes();
 void subMenuConfiguracion();
-int buscarPorID(int);
-bool modificarRegistroDeporte(Deporte, int);
-bool buscarDeporte(int);
+
+//Cadena
+void cargarCadena(char *, int);
+
+//Restaurar datos de inicio
 bool restaurarDatosInicioDeporte();
 bool restaurarDatosInicioJugadores();
 bool restaurarDatosInicio ();
+
+//Funciones para equipos
+bool agregarEquipos();
+Equipos leerRegistroEquipo(int);
+bool listarEquiposPorId();
+bool listarTodosEquipo();
+int BuscarEquiposPorID(int);
+void escribirRegistroEquipo(Equipos, int);
+bool modificarRegistroEquipo();
+bool eliminarRegistroEquipo();
+
+//Funciones para tipos de deportes
+bool agregarTipoDeporte();
+bool listarTipoPorID();
+bool listarTodosTipos();
+int buscarPorIDTipo(int);
+TipodeDeporte leeRegistroTipo (int);
+bool modificarRegistroTipodeDeporte (TipodeDeporte, int);
+bool bajaLogicaTipo();
+
+//Funciones para archivos deportes
 bool agregarDeporte();
-void mostrarDeportes ();
-bool listarDeportePorId ();
+void mostrarDeportes();
+bool listarDeportePorId();
 Deporte leeRegistro (int);
 bool modificarAnioDeOrigen();
+int buscarPorID (int);
 bool eliminarRegistroDeporte();
+bool modificarRegistroDeporte (Deporte, int);
 bool copiaSeguridadDeportes();
 bool restaurarArchDeportes();
+
+//Funciones para archivo jugadores
+bool agregarJugador();
+bool agregarJugadorValidado();
+bool listarTodos();
+bool listarPorDni();
+bool modificarFechaInscripcion();
+bool bajaLogica();
+bool buscarPorNombre();
+int buscarPorDni (int);
+Jugadores leerRegistro (int);
+bool modificarRegistro (Jugadores, int);
 bool copiaSeguridadJugadores();
 bool restaurarArchJugadores();
+
+//Otras
+void obtenerHoraSys();
+bool buscarDeporte(int);
+
 
 ///  ----------------------------------------------------  FIN PROTOTIPOS  ----------------------------------------------------  ///
 
